d1f5e3a852643f7d38e413796e225bff2cc40e90
domire8 <dominic.reber@epfl.ch>
2021-03-01 08:36:16 +0100

    update run.sh and README to pull image directly

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
+LOCAL=0
+while getopts 'l' opt; do
+    case $opt in
+        l) LOCAL=1 ;;
+        *) echo 'Error in command line parsing' >&2
+           exit 1
+    esac
+done
+shift "$(( OPTIND - 1 ))"
+
 if [ "$(uname -s)" != "Linux" ]; then
   echo "This Docker image is currently only running on Linux. Aborting..."
   exit 1
@@ -9,17 +19,32 @@ TAG=melodic-desktop
 # create a shared volume to store the ros_ws
 docker volume create --driver local \
   --opt type=none \
-  --opt device=$PWD/src \
+  --opt device="$PWD"/src \
   --opt o=bind \
-  ${NAME}_ros_ws_vol
+  "${NAME}"_ros_ws_vol
 
-xhost +
-docker run \
-  --privileged \
-  --net=host \
-  -it \
-  --rm \
-  --env="DISPLAY" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="${NAME}_ros_ws_vol:/home/ros/ros_ws/src:rw" \
-  $NAME:$TAG
+if [ "$LOCAL" -eq 1 ]; then
+  xhost +
+  docker run \
+    --privileged \
+    --net=host \
+    -it \
+    --rm \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="${NAME}_ros_ws_vol:/home/ros/ros_ws/src:rw" \
+    "$NAME:$TAG"
+else
+  PACKAGE_NAME=ghcr.io/domire8/franka-panda-rviz-visualization/franka-panda-rviz-visualization:melodic-desktop
+  docker pull $PACKAGE_NAME
+  xhost +
+  docker run \
+    --privileged \
+    --net=host \
+    -it \
+    --rm \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="${NAME}_ros_ws_vol:/home/ros/ros_ws/src:rw" \
+    "$PACKAGE_NAME"
+fi
